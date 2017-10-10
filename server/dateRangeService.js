d4014f9ee576658fd36b751ae9e596e21dd42d66
Jason Fischer <jfischer@glgroup.com>
2017-10-10 16:55:37 -0500

    updated parsing logic

--- server/dateRangeService.js
+++ server/dateRangeService.js
@@ -15,58 +15,114 @@ const parseDates = (phrase) => {
 
   return app.textRequest(phrase, options)
     .then(response => {
+
+      let validate = validateResult(response);
+      if (!validate.success)
+        return validate;
+
       let result = response.result;
-
-      //Bad result, could not parse phrase
-      if (result.actionIncomplete) {
-        return {
-          success: false,
-          msg: result.fulfillment.messages[0].speech
-        }
-      }
-
-      if (!result.parameters) {
-        return {
-          success: false,
-          msg: "Invalid query"
-        }
-      }
-
       let parameters = result.parameters;
       let dateBlocks = [];
 
-      //We expect to always have a startDate, but not the EndDate.
-      //If we have a start and end date, we will create an array of all dates between start/end date
+      //Get all the Dates w/o times
       let dates = [];
-      if (parameters.startDate && parameters.endDate) {
-        let range = moment.range(moment(parameters.startDate), moment(parameters.endDate));
-        dates = Array.from(range.by('day', { exclusive: false }));
+      if (parameters.dates.length > 1) {
+        switch(parameters.datesOperator) {
+          case "through":
+            let range = moment.range(moment(parameters.dates[0]), moment(parameters.dates[1]));
+            dates = Array.from(range.by('day', { exclusive: false }));
+            break;
+          case "and":
+          case "or":
+          default:
+            parameters.dates.forEach(dt => dates.push(moment(dt)));
+            break;
+        }
       } else {
-        dates.push(moment(parameters.startDate));
+        dates.push(moment(parameters.dates[0]));
       }
 
-      //Build out the dates appending the time ranges. It's expected that we will always
-      //have a time range (which is required by intent rules)
-      let timeRange = parameters.between.split('/');
+      //Build out the dates appending the time ranges.
+      let ts = parameters.timeSpan;
       for (let i in dates) {
         let dt = dates[i].format('YYYY-MM-DD');
-        dateBlocks.push({
-          startDate: moment(`${dt}T${timeRange[0]}`),
-          endDate: moment(`${dt}T${timeRange[1]}`)
-        })
-      }
 
-      return {
-        success: dateBlocks.length > 0,
-        msg: dateBlocks.length === 0 ? "Failed to parse dates" : null,
-        result: dateBlocks
-      };
+        //If the declaration is "between", we should treat the operation as a time-range
+        let operator = ts.operator;
+        if (ts.timeDeclaration === "between")
+            operator = "through";
+
+        switch(operator) {
+          case "and":
+          case "or":
+            //Add the date for each time since they aren't to be treated as a range
+            //TODO: Need to consolidate dates IF any time block intersect (e.g. 2p-3p and 3p-4p should resolve to 2pm-4pm)
+            ts.times.forEach(time => dateBlocks.push(createDateBlock(dt, [time])));
+            break;
+          case "through":
+          default:
+            dateBlocks.push(createDateBlock(dt, ts.times));
+            break;
+        }
+      }
+      return createResponse(true, dateBlocks);
     })
     .catch(error => {
-        return { success: false, msg: error };
+        return createResponse(false, null, error.message)
     });
 };
 
+const createDateBlock = (dt, timeRanges) => {
+  let start = moment(`${dt}T${timeRanges[0]}`);
+  let end;
+
+  if (timeRanges.length < 2) {
+    //We don't have a time-range, so default it to an hour
+    end = moment(start).add(1, 'hour');
+  } else {
+    end = moment(`${dt}T${timeRanges[1]}`)
+  }
+
+  return {
+    startDate: start,
+    endDate: end
+  }
+};
+
+const validateResult = (response) => {
+  let result = response.result;
+
+  //Bad result, could not parse phrase
+  if (result.actionIncomplete) {
+    return createResponse(false, null, result.fulfillment.messages[0].speech)
+  }
+
+  if (!result.parameters) {
+    return createResponse(false, null, "Could not parse");
+  }
+
+  let parameters = result.parameters;
+
+  if (!parameters.dates || parameters.dates.length === 0) {
+    return createResponse(false, null, "No Dates found")
+  }
+
+  if (!parameters.timeSpan ||
+    !parameters.timeSpan.times ||
+    !parameters.timeSpan.times.length === 0) {
+    return createResponse(false, null, "No times found")
+  }
+
+  return createResponse(true);
+};
+
+const createResponse = (success, result, msg) => {
+  return {
+    success: success,
+    msg: msg,
+    result: result
+  };
+};
 
 module.exports = {
   parseDates
